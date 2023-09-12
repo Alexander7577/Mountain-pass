ad10da3c8a2bddf5364cde6aeea34677ee3dbbad
Alexander7577 <ChudAlex2004@bk.ru>
2023-09-12 14:35:04 +0700

    State
    Add state

--- MountainPass/sportapp/views.py
+++ MountainPass/sportapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .serializers import PerevalAddedSerializer, PerevalUpdateSerializer
 from .models import PerevalAdded
 
@@ -11,3 +11,45 @@ class SubmitData(viewsets.ModelViewSet):
         if self.action == 'update':
             return PerevalUpdateSerializer
         return PerevalAddedSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = PerevalAddedSerializer(data=request.data)
+        instance = self.get_object()
+        if serializer.is_valid():
+            serializer.save()
+            updated_instance = PerevalAdded.objects.get(pk=instance.pk)
+            return Response(
+                {
+                    'state': '1',
+                    'message': 'Объект успешно создан',
+                    'data': PerevalAddedSerializer(updated_instance).data
+                }
+            )
+        return Response(
+            {
+                'state': '2',
+                'message': 'Не удалось создать объект',
+                'data': serializer.errors
+            }
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = PerevalUpdateSerializer(instance, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            updated_instance = PerevalAdded.objects.get(pk=instance.pk)
+            return Response(
+                {
+                    'state': '1',
+                    'message': 'Изменения успешно применены',
+                    'data': PerevalAddedSerializer(updated_instance).data
+                }
+            )
+        return Response(
+            {
+                'state': '0',
+                'message': 'Не удалось применить изменения',
+                'errors': serializer.errors
+            }
+        )
